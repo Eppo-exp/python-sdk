4ee7bb62a73759e2354843f46af0dac85eff7ae4
Peter Loomis <loomispeter@gmail.com>
2022-05-10 17:32:07 -0700

    v0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,3 +10,8 @@
 #### Deprecated:
   * Describe deprecated APIs in this version
 -->
+
+## [0.0.3] - 2022-05-11
+
+#### New Features
+* Implemented allow list for subject-variation overrides
--- eppo_client/__init__.py
+++ eppo_client/__init__.py
@@ -10,7 +10,7 @@ from eppo_client.constants import MAX_CACHE_ENTRIES
 from eppo_client.http_client import HttpClient, SdkParams
 from eppo_client.read_write_lock import ReadWriteLock
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 __client: Optional[EppoClient] = None
 __lock = ReadWriteLock()
