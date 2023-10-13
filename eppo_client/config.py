4ff76364ab4dab66e0a7cf0195dec492c98126f4
Leo Romanovsky <leoromanovsky@users.noreply.github.com>
2023-10-13 09:25:36 -0700

    add try except to all public client methods (FF-947) (#26)
    * add try except to all public client methods (FF-947)
    
    * test all methods
    
    * value error in all public methods

--- eppo_client/config.py
+++ eppo_client/config.py
@@ -8,6 +8,7 @@ class Config(SdkBaseModel):
     api_key: str
     base_url: str = "https://fscdn.eppo.cloud/api"
     assignment_logger: AssignmentLogger
+    is_graceful_mode: bool = True
 
     def _validate(self):
         validate_not_blank("api_key", self.api_key)
--- test/client_test.py
+++ test/client_test.py
@@ -252,6 +252,45 @@ def test_with_null_experiment_config(mock_config_requestor):
     assert client.get_assignment("user-1", "experiment-key-1") is None
 
 
+@patch("eppo_client.configuration_requestor.ExperimentConfigurationRequestor")
+@patch.object(EppoClient, "get_assignment_variation")
+def test_graceful_mode_on(mock_get_assignment_variation, mock_config_requestor):
+    mock_get_assignment_variation.side_effect = Exception("This is a mock exception!")
+
+    client = EppoClient(
+        config_requestor=mock_config_requestor,
+        assignment_logger=AssignmentLogger(),
+        is_graceful_mode=True,
+    )
+
+    assert client.get_assignment("user-1", "experiment-key-1") is None
+    assert client.get_boolean_assignment("user-1", "experiment-key-1") is None
+    assert client.get_json_string_assignment("user-1", "experiment-key-1") is None
+    assert client.get_numeric_assignment("user-1", "experiment-key-1") is None
+    assert client.get_string_assignment("user-1", "experiment-key-1") is None
+    assert client.get_parsed_json_assignment("user-1", "experiment-key-1") is None
+
+
+@patch("eppo_client.configuration_requestor.ExperimentConfigurationRequestor")
+@patch.object(EppoClient, "get_assignment_variation")
+def test_graceful_mode_off(mock_get_assignment_variation, mock_config_requestor):
+    mock_get_assignment_variation.side_effect = Exception("This is a mock exception!")
+
+    client = EppoClient(
+        config_requestor=mock_config_requestor,
+        assignment_logger=AssignmentLogger(),
+        is_graceful_mode=False,
+    )
+
+    with pytest.raises(Exception):
+        client.get_assignment("user-1", "experiment-key-1")
+        client.get_boolean_assignment("user-1", "experiment-key-1")
+        client.get_json_string_assignment("user-1", "experiment-key-1")
+        client.get_numeric_assignment("user-1", "experiment-key-1")
+        client.get_string_assignment("user-1", "experiment-key-1")
+        client.get_parsed_json_assignment("user-1", "experiment-key-1")
+
+
 @pytest.mark.parametrize("test_case", test_data)
 def test_assign_subject_in_sample(test_case):
     print("---- Test case for {} Experiment".format(test_case["experiment"]))
