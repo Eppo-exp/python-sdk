2c80bca547cd862fc324c3e341c76388e7c2dcd6
Leo Romanovsky <leo@geteppo.com>
2024-02-16 10:09:40 -0800

    bump to 1.3.1

--- eppo_client/__init__.py
+++ eppo_client/__init__.py
@@ -10,7 +10,7 @@ from eppo_client.constants import MAX_CACHE_ENTRIES
 from eppo_client.http_client import HttpClient, SdkParams
 from eppo_client.read_write_lock import ReadWriteLock
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 __client: Optional[EppoClient] = None
 __lock = ReadWriteLock()
