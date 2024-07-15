addc822e857c1f432e36d0634e10d5d062202795
Leo Romanovsky <leo@geteppo.com>
2024-07-15 11:25:05 -0700

    leave default at 5 min; add comment for breaking change in future

--- eppo_client/constants.py
+++ eppo_client/constants.py
@@ -1,3 +1,6 @@
 # poller
 POLL_JITTER_SECONDS = 2
-POLL_INTERVAL_SECONDS = 10
+# We accidently shipped Python with a 5 minute poll interval.
+# Customers can set the poll interval to 30 seconds to match the behavior of the other server SDKs.
+# Please change this to 30 seconds when ready to bump to 4.0.
+POLL_INTERVAL_SECONDS = 5 * 60  # 5 minutes
