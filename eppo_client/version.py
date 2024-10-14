2f7bae8bcbc4ec9924dae46b7f7fb54b47c6af90
Sameeran Kunche <skunche@gmail.com>
2024-10-14 15:38:19 -0700

    Update package version

--- eppo_client/version.py
+++ eppo_client/version.py
@@ -1,4 +1,1 @@
-# Note to developers: When ready to bump to 4.0, please change
-# the `POLL_INTERVAL_SECONDS` constant in `eppo_client/constants.py`
-# to 30 seconds to match the behavior of the other server SDKs.
-__version__ = "3.8.0"
+__version__ = "4.0.2"
